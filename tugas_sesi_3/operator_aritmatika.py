e7391ad8438bc9a0c9e80b75d56a7958708d4c62
NicholasPV <103667899+NicholasPV@users.noreply.github.com>
2022-10-03 21:37:33 +0700

    Operator Aritmatika (+, -, *, /)

---
+++ tugas_sesi_3/operator_aritmatika.py
@@ -0,0 +1,14 @@
+#Operator Aritmatika
+numA = int(input("Masukkan angka pertama: "))
+numB = int(input("Masukkan angka kedua: "))
+
+penjumlahan = numA + numB
+pengurangan = numA - numB
+perkalian = numA * numB
+pembagian = numA / numB
+
+print("\n")
+print("Hasil Penjumlahan = ", penjumlahan)
+print("Hasil Pengurangan = ", pengurangan)
+print("Hasil Perkalian = ", perkalian)
+print("Hasil Pembagian = ", pembagian)
