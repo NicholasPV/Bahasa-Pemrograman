7332b39870b0d3efc843f1decb8a88ed8ef76295
NicholasPV <103667899+NicholasPV@users.noreply.github.com>
2022-10-20 20:41:30 +0700

    Looping

---
+++ tugas_sesi_5/Looping.py
@@ -0,0 +1,21 @@
+# Looping with Range
+for var in range(3):
+    print(var)
+
+for a in "Pemrograman":
+    if a == "m":
+        print(a)
+
+for b in "Pemrograman":
+    if b == "g":
+        break
+    print(b)
+
+for c in "Pemrograman":
+    if c == "r":
+        continue
+    print(c)
+
+# Length
+d = "Pemrograman"
+print(len(d))
