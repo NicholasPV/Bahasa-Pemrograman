436cf63f2dfeb16da3386cc0f7d38739d6cc0b57
NicholasPV <103667899+NicholasPV@users.noreply.github.com>
2022-10-31 10:31:00 +0700

    Function

---
+++ tugas_sesi_6/Function.py
@@ -0,0 +1,18 @@
+# Definisi Function
+
+# Header
+def kepala():
+    print("Nama: Nicholas Patrick Varian")
+    print("NIM: 20210801102")
+
+# Body
+def jumlah():
+    print("Masukkan Angka 1: ")
+    print("Masukkan Angka 2: ")
+
+# Footer
+def kaki():
+    print("Terima Kasih")
+
+while True:
+    kepala()
